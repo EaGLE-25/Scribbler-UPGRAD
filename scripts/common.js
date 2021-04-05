ea7812dc976195be8024aade96b77636588abfb0
Sai Darshan <saidarshan27@gmail.com>
2021-04-05 21:34:34 +0530

    add signin modal and homepage buttons

--- scripts/common.js
+++ scripts/common.js
@@ -1,17 +1,38 @@
 let signinBtn = document.querySelector(".signin");
 let signupBtn = document.querySelector(".signup");
 let modalCloseBtn = document.querySelector(".modal-close");
-let modalContainer = document.querySelector(".modal-container");
-
+let signupModal = document.querySelector(".signup-modal");
+let signinModal = document.querySelector(".signin-modal");
+let notMemberSignUp = document.querySelector(".not-member .signup-link");
 
 
 signupBtn.addEventListener("click",(e)=>{
-	modalContainer.classList.remove("hide");
-	modalContainer.classList.add("show");
+	openModal(signupModal);
 })
 
-modalCloseBtn.addEventListener("click",(e)=>{
-	modalContainer.classList.add("hide");
-	modalContainer.classList.remove("show");
+signinBtn.addEventListener("click",(e)=>{
+	openModal(signinModal);
 })
 
+notMemberSignUp.addEventListener("click",(e)=>{
+	e.preventDefault();
+	closeModal(signinModal);
+	openModal(signupModal);
+})
+
+
+function openModal(modal){
+	modal.classList.remove("hide");
+	modal.classList.add("show");
+}
+
+function closeModalById(modalID){
+	let modal = document.getElementById(modalID); 
+	modal.classList.add("hide");
+	modal.classList.remove("show");
+}
+
+function closeModal(modal){
+	modal.classList.add("hide");
+	modal.classList.remove("show");
+}
